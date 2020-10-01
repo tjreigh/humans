b014804a8a5f2968d1fbe66d144ae383c54c6e6f
TJDoesCode <tjdoescode@gmail.com>
2020-10-01 15:00:39 -0500

    broken: control panel

--- private/login.js
+++ private/login.js
@@ -1,15 +1,25 @@
-const formEvent = (e) => {
+const toggleLoader = (disp) => {
+	const loader = document.querySelector('#loader');
+	console.log('loading');
+	const display = disp ? 'block' : 'none';
+	loader.style.display = display;
+};
+
+const loginEvent = (e) => {
 	e.preventDefault();
 
 	const pass = document.querySelector('input#pass').value;
 
+	toggleLoader(true);
+
 	fetch(`https://lhs-humans-auth.glitch.me/auth?pass=${pass}`)
 		.then(res => res.text())
 		.then(data => {
+			toggleLoader(false);
 			console.log(data);
 			document.body.innerHTML = data;
 		});
 };
 
 const form = document.querySelector('form#login');
-form.addEventListener('submit', formEvent.bind(this));
+form.addEventListener('submit', loginEvent.bind(this));
